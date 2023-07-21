afcf757cb5796f4e000976e21a486b1a3bc3408b
rathodvilas1994 <135522799+rathodvilas1994@users.noreply.github.com>
2023-07-21 14:43:38 +0530

    [vilas]largest number from 2 no.

---
+++ src/programP/Q2.java
@@ -0,0 +1,20 @@
+package programP;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		if(a>b) 
+		{
+			System.out.println("a is greater");
+		}
+		else 
+		{
+			System.out.println("b is greater");
+		}
+
+	}
+
+}
